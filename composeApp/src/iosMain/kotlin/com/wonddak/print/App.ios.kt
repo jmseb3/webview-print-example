e0551bb49b3b6c8d46dd81adb9f4fd01fd4bc309
jmseb3 <jmseb3@naver.com>
2024-07-01 18:17:01 +0900

    IOS 프린트 설정 추가

--- composeApp/src/iosMain/kotlin/com/wonddak/print/App.ios.kt
+++ composeApp/src/iosMain/kotlin/com/wonddak/print/App.ios.kt
@@ -6,9 +6,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.UIKitView
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ObjCSignatureOverride
 import platform.Foundation.NSMutableURLRequest
 import platform.Foundation.NSURL
+import platform.UIKit.UIPrintInfo
+import platform.UIKit.UIPrintInfoOutputType
+import platform.UIKit.UIPrintInteractionController
+import platform.UIKit.viewPrintFormatter
+import platform.WebKit.WKNavigation
 import platform.WebKit.WKNavigationDelegateProtocol
+import platform.WebKit.WKScriptMessage
+import platform.WebKit.WKScriptMessageHandlerProtocol
+import platform.WebKit.WKUserContentController
 import platform.WebKit.WKWebView
 import platform.darwin.NSObject
 
@@ -19,6 +28,10 @@ actual fun WebView(modifier: Modifier) {
     val webView = remember {
         WKWebView().apply {
             configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+            configuration.userContentController.addScriptMessageHandler(
+                PrintDelegate(this),
+                "printM"
+            )
             navigationDelegate = WebDelegate()
         }
     }
@@ -38,5 +51,47 @@ actual fun WebView(modifier: Modifier) {
 }
 
 class WebDelegate() : NSObject(), WKNavigationDelegateProtocol {
+    @ObjCSignatureOverride
+    override fun webView(webView: WKWebView, didFinishNavigation: WKNavigation?) {
+        webView.evaluateJavaScript(
+            "window.print=()=>{window.webkit.messageHandlers.printM.postMessage(document.title);}",
+            null
+        )
+    }
 
+}
+
+class PrintDelegate(
+    private val webView: WKWebView,
+) : NSObject(), WKScriptMessageHandlerProtocol {
+    override fun userContentController(
+        userContentController: WKUserContentController,
+        didReceiveScriptMessage: WKScriptMessage,
+    ) {
+        if (didReceiveScriptMessage.name == "printM") {
+            runCatching {
+                didReceiveScriptMessage.body() as String
+            }.onSuccess { printName ->
+                val printController = webView.viewPrintFormatter()
+
+                val printInfo = UIPrintInfo.printInfoWithDictionary(null)
+                printInfo.outputType = UIPrintInfoOutputType.UIPrintInfoOutputGeneral
+                printInfo.jobName = printName
+
+                val printInteractionController =
+                    UIPrintInteractionController.sharedPrintController()
+                printInteractionController.printInfo = printInfo
+                printInteractionController.printFormatter = printController
+                printInteractionController.presentAnimated(true) { controller, completed, error ->
+                    if (completed) {
+                        print("Printing completed")
+                    } else {
+                        error?.let { err ->
+                            print("Printing failed with error: ${err.localizedDescription}")
+                        } ?: print("Printing was canceled")
+                    }
+                }
+            }
+        }
+    }
 }
